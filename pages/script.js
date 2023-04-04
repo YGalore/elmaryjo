c14c2d0b14156f3639700dee8d1b258e03eda414
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2023-04-04 17:07:53 +0300

    Create overlay

---
+++ pages/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const hiddenBox = document.querySelector(".mob-nav");
+
+menu.addEventListener('click', () => {
+    //alert("am clicked now");
+    hiddenBox.classList.display.ad("block");
+});z
